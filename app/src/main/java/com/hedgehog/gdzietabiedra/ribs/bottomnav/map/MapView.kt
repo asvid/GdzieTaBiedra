fbdcde2f84ceff42931c370bf41cffdb47d2da85
Adam Świderski <adam.swiderski89@gmail.com>
2018-09-03 23:06:05 +0200

    minor changes

--- app/src/main/java/com/hedgehog/gdzietabiedra/ribs/bottomnav/map/MapView.kt
+++ app/src/main/java/com/hedgehog/gdzietabiedra/ribs/bottomnav/map/MapView.kt
@@ -3,26 +3,21 @@ package com.hedgehog.gdzietabiedra.ribs.bottomnav.map
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
-import kotlinx.android.synthetic.main.map_rib.view.*
+import kotlinx.android.synthetic.main.map_rib.view.map_view
 import timber.log.Timber
 
 /**
  * Top level view for {@link MapBuilder.MapScope}.
  */
 class MapView @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
 ) : FrameLayout(context, attrs, defStyle), MapInteractor.MapPresenter {
 
-    override fun initView() {
+  override fun initView() {
+    map_view.onCreate(null)
+    map_view.getMapAsync {
+      Timber.d("map is loaded")
+      map_view.onResume()
     }
-
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-        map_view.onCreate(null)
-        map_view.getMapAsync {
-            Timber.d("map is loaded")
-            map_view.onResume()
-            it.setMyLocationEnabled(true)
-        }
-    }
+  }
 }
