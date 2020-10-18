39e04eeac9f7cb0916e09e0a4eaed63b2484a706
Adam Świderski <adam.swiderski89@gmail.com>
2020-10-18 12:17:20 +0200

    New activity added

--- buildSrc/src/main/java/ProjectDeps.kt
+++ buildSrc/src/main/java/ProjectDeps.kt
@@ -4,10 +4,6 @@ object ProjectDeps {
   const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
   const val kotlinExtensions = "org.jetbrains.kotlin:kotlin-android-extensions:${Versions.kotlin}"
   const val firebasePlugin = "com.google.firebase:firebase-plugins:1.0.5"
-  const val dexcount = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:1.0.2"
-  const val apkSize = "com.vanniktech:gradle-android-apk-size-plugin:0.4.0"
-  const val coveralls = "org.kt3k.gradle.plugin:coveralls-gradle-plugin:2.8.1"
-  const val jacoco = "com.dicedmelon.gradle:jacoco-android:0.1.4"
   const val crashlytics = "io.fabric.tools:gradle:1.28.0"
   const val dokka = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.17"
 }
