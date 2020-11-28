fbc6d3f6f7e2add6dab57ede16d13f0959ce2758
Adam Świderski <adam.swiderski89@gmail.com>
2020-11-28 12:37:57 +0100

    Develop (#60)
    * removed unused config files
    
    * Feature/#53/settings page (#54)
    
    * settings page with notification settings and app info
    
    * added notification service, job etc.; beginning of handling settings changes
    
    * better search query (#56)
    
    * Feature/updated translations (#57)
    
    * better search query
    
    * Updated translations and version number
    
    * Update Build.kt
    
    * Feature/updated translations (#59)
    
    * better search query
    
    * Updated translations and version number
    
    * Updated missing translations, changed accent color a bit to make app look better
    
    * updated notification texts

--- app/src/main/java/com/hedgehog/gdzietabiedra/utils/ExtensionMethods.kt
+++ app/src/main/java/com/hedgehog/gdzietabiedra/utils/ExtensionMethods.kt
@@ -1,24 +1,14 @@
 package com.hedgehog.gdzietabiedra.utils
 
+import android.content.Context
 import com.github.asvid.biedra.domain.Location
+import com.github.asvid.biedra.domain.SundayShopping
 import com.google.android.gms.maps.model.LatLng
+import org.joda.time.LocalDate
+import java.text.DateFormat
 import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
-
-fun Int.toLocalNumberFormat(locale: Locale): String {
-    return NumberFormat.getNumberInstance(locale).format(this)
-}
-
-fun String.toDate(format: String): Date? {
-    val formatter = SimpleDateFormat(format, Locale("pl"))
-    return formatter.parse(this)
-}
-
-fun Date.print(format: String): String {
-    val dateFormat = SimpleDateFormat(format, Locale("pl"))
-    return dateFormat.format(this)
-}
 
 fun Double.round(decimals: Int): Double {
     var multiplier = 1.0
@@ -29,3 +19,9 @@ fun Double.round(decimals: Int): Double {
 fun Location.toLatLng(): LatLng = LatLng(this.lat, this.lng)
 
 fun LatLng.toPosition(): Location = Location(this.latitude, this.longitude)
+
+fun LocalDate.print(context: Context): String {
+    val date = this.toDate()
+    val dateFormat: DateFormat = android.text.format.DateFormat.getDateFormat(context)
+    return dateFormat.format(date)
+}
